80be8dc59a7e48310d6c3081fc7d736bf27d01cd
Valeriu Cojocari <valeriu.cojocari@alvys.com>
2024-06-05 11:02:06 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        var piEstimate1000 = EstimatePi(1000);
+        var piEstimate10000 = EstimatePi(10000);
+        var piEstimate100000 = EstimatePi(100000);
+        var piEstimate1000000 = EstimatePi(1000000);
+
+        Console.WriteLine($"Estimarea pentru {1000} mostre este: {piEstimate1000}");
+        Console.WriteLine($"Estimarea pentru {10000} mostre este: {piEstimate10000}");
+        Console.WriteLine($"Estimarea pentru {100000} mostre este: {piEstimate100000}");
+        Console.WriteLine($"Estimarea pentru {1000000} mostre este: {piEstimate1000000}");
+
+    }
+
+    static double EstimatePi(int numSamples)
+    {
+        var numPointsInCircle = 0;
+        var rand = new Random();
+
+        for (int i = 0; i < numSamples; i++)
+        {
+            var x = rand.NextDouble() * 2 - 1;
+            var y = rand.NextDouble() * 2 - 1;
+
+            if (x * x + y * y <= 1)
+            {
+                numPointsInCircle++;
+            }
+        }
+
+        return 4.0 * numPointsInCircle / numSamples;
+    }
+}
